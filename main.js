d5782becf40f27d148f499f46c1316816f2bd651
mathgoms02 <math.gomsx3@gmail.com>
2026-05-04 21:10:31 -0300

    refactor(js): Refactored all main.js

--- main.js
+++ main.js
@@ -12,39 +12,6 @@ let wordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
 
-// Carrousel Variabels
-const slide = document.querySelector(".carousel-slide");
-const items = document.querySelectorAll(".slide");
-const slideTextBox = document.querySelectorAll(".slide-text");
-const nextBtn = document.getElementById("nextBtn");
-const prevBtn = document.getElementById("prevBtn");
-const carouselGuideDotContainer = document.querySelector(".carousel-guide-dot");
-
-let counter = 0;
-let size = items[0].getBoundingClientRect().width;
-
-// Initial Position
-slide.style.transform = "translateX(0px)";
-slideTextBox[0].classList.add("fade-in");
-
-function debounce(func, timeout = 300) {
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func(...args);
-    }, timeout);
-  };
-}
-
-window.addEventListener(
-  "resize",
-  debounce(() => {
-    size = items[0].getBoundingClientRect().width;
-    slide.style.transform = `translateX(${-size * counter}px)`;
-  }),
-);
-
 // Typewriter
 function typewriter() {
   const currentWord = words[wordIndex];
@@ -69,62 +36,100 @@ function typewriter() {
     wordIndex = (wordIndex + 1) % words.length; // Volta para o inicio da lista
     typeSpeed = 500;
   }
-
   setTimeout(typewriter, typeSpeed);
 }
-// DOMContentLoaded Dispara quando o html inicial é carregado
+
+// DOMContentLoaded event to when initial html is loaded
 document.addEventListener("DOMContentLoaded", typewriter);
 
+// Carrousel Variabels
+const carouselTrack = document.querySelector(".carousel-slide");
+const slides = document.querySelectorAll(".slide");
+const slideTexts = document.querySelectorAll(".slide-text");
+
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+
+const dotsContainer = document.querySelector(".carousel-guide-dot");
+
+let currentIndex = 0;
+let slideWidth = slides[0].getBoundingClientRect().width;
+
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, timeout);
+  };
+}
+
+window.addEventListener(
+  "resize",
+  debounce(() => {
+    slideWidth = slides[0].getBoundingClientRect().width;
+    carouselTrack.style.transform = `translateX(${-slideWidth * currentIndex}px)`;
+  }),
+);
+
 // Carrousel
-items.forEach(() => {
-  const newDot = document.createElement("i");
-  newDot.classList.add("fa-solid", "fa-circle", "dot");
-  carouselGuideDotContainer.appendChild(newDot);
-});
+function initCarousel() {
+  // Initial Position
+  carouselTrack.style.transform = "translateX(0px)";
+  slideTexts[0].classList.add("fade-in");
 
-const carouselGuideDot = document.querySelectorAll(".dot");
+  createDots();
+  updateDots(0);
+}
 
-function dotColor() {
-  carouselGuideDot.forEach((item) => item.classList.remove("dot-active"));
-  carouselGuideDot[counter].classList.add("dot-active");
+let dots = [];
+
+function createDots() {
+  slides.forEach(() => {
+    const newDot = document.createElement("i");
+    newDot.classList.add("fa-solid", "fa-circle", "dot");
+    dotsContainer.appendChild(newDot);
+  });
+  dots = document.querySelectorAll(".dot");
+}
+
+function updateDots(index) {
+  dots.forEach((dot) => dot.classList.remove("dot-active"));
+  dots[index].classList.add("dot-active");
+}
+
+function moveSlide() {
+  carouselTrack.style.transform = `translateX(${-slideWidth * currentIndex}px)`;
+}
+
+function updateCarousel(newIndex) {
+  const lastIndex = currentIndex;
+  currentIndex = newIndex;
+
+  moveSlide();
+
+  slideTexts[lastIndex].classList.add("fade-out");
+  slideTexts[currentIndex].classList.add("fade-in");
+
+  setTimeout(() => {
+    slideTexts[lastIndex].classList.remove("fade-in");
+    slideTexts[lastIndex].classList.remove("fade-out");
+  }, 500);
+
+  updateDots(currentIndex);
 }
 
 nextBtn.addEventListener("click", () => {
-  if (counter >= items.length - 1) return;
-  counter++;
-
-  const currentText = document.querySelector(`.sl${counter + 1}`);
-  const lastText = document.querySelector(`.sl${counter}`);
-
-  slide.style.transform = `translateX(${-size * counter}px)`;
-
-  lastText.classList.add("fade-out");
-  currentText.classList.add("fade-in");
-
-  setTimeout(() => {
-    lastText.classList.remove("fade-in");
-    lastText.classList.remove("fade-out");
-  }, 500);
-  dotColor(counter);
+  if (currentIndex < slides.length - 1) {
+    updateCarousel(currentIndex + 1);
+  }
 });
 
 prevBtn.addEventListener("click", () => {
-  if (counter <= 0) return;
-  counter--;
-
-  const currentText = document.querySelector(`.sl${counter + 1}`);
-  const lastText = document.querySelector(`.sl${counter + 2}`);
-
-  slide.style.transform = `translateX(${-size * counter}px)`;
-
-  lastText.classList.add("fade-out");
-  currentText.classList.add("fade-in");
-
-  setTimeout(() => {
-    lastText.classList.remove("fade-in");
-    lastText.classList.remove("fade-out");
-  }, 500);
-  dotColor(counter);
+  if (currentIndex > 0) {
+    updateCarousel(currentIndex - 1);
+  }
 });
 
-dotColor(counter);
+initCarousel();
